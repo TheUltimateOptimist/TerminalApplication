9257ca85dfb407cb5675c84ed6aafdac88820af4
TheUltimateOptimist <digitalConfidence@web.de>
2021-08-20 22:52:18 +0200

    fixed bugs in finance part

--- Tables/finance.py
+++ Tables/finance.py
@@ -1,9 +1,10 @@
-from GAUD import get,add
+from GAUD import get, add
 import format
 import sql
 import color
 import time
 import functions
+
 
 def makeTransaction(showOperation, sqloperation, id):
     # id = 1 -> sell
@@ -17,7 +18,8 @@ def makeTransaction(showOperation, sqloperation, id):
             dateTime = f"'{input('When was the transaction made? ')}'"
         else:
             dateTime = format.currentDateTime()
-        add.addIntern("transactions", ["all except id"], [medium, reason, classification, amount, dateTime], showOperation)
+        add.addIntern("transactions", ["transaction_medium", "transaction_reason", "transaction_classification",
+                      "transaction_amount", "transaction_datetime"], [medium, reason, classification, amount, dateTime], showOperation)
         if sqloperation.__contains__("-r"):
             makeTransaction(showOperation, sqloperation, id)
 
@@ -27,23 +29,29 @@ def buy(showOperation, sqloperation):
 
 
 def sell(showOperation, sqloperation):
-    makeTransaction(showOperation, sqloperation, 1)    
+    makeTransaction(showOperation, sqloperation, 1)
+
 
 def printBalance(balance, type):
-    if balance >= 0:
+    if balance == "None" or float(balance) >= 0:
         color.printGreen(f"Your {type} balance: {balance}")
     else:
-        color.printRed(f"Your {type} balance: {balance}")    
+        color.printRed(f"Your {type} balance: {balance}")
 
 
 def getbalance(showOperation, sqloperation):
-    balance = sql.execute("SELECT SUM(transaction_amount) FROM transactions")[0][0]
-    printBalance(sum, "overall")
+    s = "2.3"
+    balance = float(sql.execute(
+        "SELECT SUM(transaction_amount) FROM transactions", showOperation, "get")[0][0])
+    printBalance(balance, "overall")
     print("")
     if sqloperation.__contains__("-sector"):
-        cashBalance = sql.execute("SELECT SUM(transaction_amount) FROM transactions WHERE transaction_medium = 'cash'")[0][0]
-        debtBalance = sql.execute("SELECT SUM(transaction_amount) FROM transactions WHERE transaction_medium = 'debt'")
-        bankBalance = sql.execute("SELECT SUM(transaction_amount) FROM transactions WHERE tansaction_medium = 'bank'")
+        cashBalance = sql.execute(
+            "SELECT SUM(transaction_amount) FROM transactions WHERE transaction_medium = 'cash'", showOperation, "get")[0][0]
+        debtBalance = sql.execute(
+            "SELECT SUM(transaction_amount) FROM transactions WHERE transaction_medium = 'debt'", showOperation, "get")[0][0]
+        bankBalance = sql.execute(
+            "SELECT SUM(transaction_amount) FROM transactions WHERE transaction_medium = 'bank'", showOperation, "get")[0][0]
         printBalance(cashBalance, "cash")
         printBalance(debtBalance, "debt")
         printBalance(bankBalance, "bank")
@@ -52,23 +60,13 @@ def getbalance(showOperation, sqloperation):
 def gettransactions(showOperation, sqloperation):
     if sqloperation.__contains__("-d"):
         days = int(input("how many days do you want to go back? "))
-        dateTime =  time.localtime(time.time() - 86400*days)
-        dateTime = f"'{dateTime.tm_year}-{dateTime.tm_mon}-{dateTime.tm_mday} {dateTime.tm_hour}:{dateTime.tm_min}:{dateTime.tm_sec}'" 
+        dateTime = time.localtime(time.time() - 86400*days)
+        dateTime = f"'{dateTime.tm_year}-{dateTime.tm_mon}-{dateTime.tm_mday} {dateTime.tm_hour}:{dateTime.tm_min}:{dateTime.tm_sec}'"
     else:
         dateTime = format.currentDateTime()
-    table = get.getIntern("transactions", ["all"], f"transaction_datetime > {dateTime}", showOperation)
+    table = get.getIntern(
+        "transactions", ["all"], f"transaction_datetime > {dateTime}", showOperation)
     functions.printTable(table, ["all"], "blue", "cyan", "blue")
-    balance = sql.execute(f"SELECT SUM(transaction_amount) FROM transactions WHERE transaction_datetime > {dateTime}")[0][0]
+    balance = float(sql.execute(
+        f"SELECT SUM(transaction_amount) FROM transactions WHERE transaction_datetime > {dateTime}", showOperation, "get")[0][0])
     printBalance(balance, "overall")
-              
-
-
-
-        
-
-
-
-
-
-        
-
